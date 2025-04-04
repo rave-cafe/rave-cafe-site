4b1ed45a4fed98483f78e663e67183511681af86
Dalton Saffe <daltonsaffe@gmail.com>
2025-04-04 16:54:11 -0400

    add tailwind, refactor to src, form test

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,40 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("bundle.css");
-};
+import fs from "fs";
+import path from "path";
+
+import cssnano from "cssnano";
+import postcss from "postcss";
+import tailwindcss from "@tailwindcss/postcss";
+
+export default function (eleventyConfig) {
+  eleventyConfig.on("eleventy.before", async () => {
+    const tailwindInputPath = path.resolve("./src/assets/index.css");
+
+    const tailwindOutputPath = "./_site/assets/index.css";
+
+    const cssContent = fs.readFileSync(tailwindInputPath, "utf8");
+
+    const outputDir = path.dirname(tailwindOutputPath);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const result = await processor.process(cssContent, {
+      from: tailwindInputPath,
+      to: tailwindOutputPath,
+    });
+
+    fs.writeFileSync(tailwindOutputPath, result.css);
+  });
+
+  const processor = postcss([
+    tailwindcss(),
+
+    cssnano({
+      preset: "default",
+    }),
+  ]);
+
+  return {
+    dir: { input: "src", output: "_site" },
+  };
+}
